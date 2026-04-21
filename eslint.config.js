3336d7b7be4f6670e59226cfb2ad3de3849dcb56
Mariya Britvina <m.sea7@mail.ru>
2026-04-21 17:31:46 +0300

    Add eslint

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+
+const react = require('eslint-plugin-react');
+const globals = require('globals');
+
+module.exports = [
+  {
+    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+    plugins: {
+      react,
+    },
+    ...react.configs.flat.recommended,
+    languageOptions: {
+      ...react.configs.flat.recommended.languageOptions,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {
+      'react/jsx-uses-react': 'error',
+      'react/jsx-uses-vars': 'error',
+     },
+  },
+];
