4b93a3b1b1c8d895a5e4698855b2016f9d36b17e
Mariya Britvina <m.sea7@mail.ru>
2026-05-12 02:38:17 +0300

    Ввод данных пассажира с простой валидацией

--- src/component/Passengers.jsx
+++ src/component/Passengers.jsx
@@ -1,10 +1,83 @@
-import { NavLink } from "react-router";
+import { useState } from 'react';
+import './Passengers.css';
+import { useNavigate } from 'react-router';
 
 const Passengers = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        lastName: '',
+        thirdName: '',
+        birthDate: '',
+        serial: '',
+        number: '',
+    });
+    
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        console.log(e.target.name);
+        console.log(e.target.value);
+        const { name, value } = e.target;
+        setFormData(prev => ({ ...prev, [name]: value }));
+    };
+
+    const handleNext = (e) => {
+        e.preventDefault();
+        
+        if (validate()) {
+            navigate('/payment');
+        }
+    }
+
+    const validate = () => {
+        return formData.serial.length === 4 && formData.number.length === 6 && formData.birthDate ;
+    }
+
+    const isFormValid = validate();
+
     return (
-        <div className="main">
-            Passengers
-            <NavLink to="/payment">pay</NavLink>
+        <div className="passengers">
+            <form className="passenger" onSubmit={handleNext}>
+                <label className="passenger-label">
+                    <input name="type" className="passenger-input" type="text" value="Взрослый" disabled></input>
+                </label>
+                <div className="passenger-data">
+                    <label className="passenger-label">
+                        <div>Фамилия</div>
+                        <input name="lastName" className="passenger-input" type="text" value={formData.lastName} required onChange={handleChange}></input>
+                    </label>
+                    <label className="passenger-label">
+                        <div>Имя</div>
+                        <input name="name" className="passenger-input" type="text" value={formData.name} required onChange={handleChange}></input>
+                    </label>
+                    <label className="passenger-label">
+                        <div>Отчество</div>
+                        <input name="thirdName" className="passenger-input" type="text" value={formData.thirdName} required onChange={handleChange}></input>
+                    </label>
+                    <label className="passenger-label">
+                        <div>Дата рождения</div>
+                        <input name="birthDate" className="passenger-input" type="date" value={formData.birthDate} required onChange={handleChange}></input>
+                    </label>
+                </div>
+                <div className="passenger-identity">
+                    <label className="passenger-label">
+                        <div>Тип докуммента</div>
+                        <input name="documentType" className="passenger-input" type="calendar" value="Паспорт РФ" disabled></input>
+                    </label>
+                    <label className="passenger-label">
+                        <div>Серия</div>
+                        <input name="serial" className="passenger-input" type="number" value={formData.serial} required onChange={handleChange}></input>
+                    </label>
+                    <label className="passenger-label">
+                        <div>Номер</div>
+                        <input name="number" className="passenger-input" type="number" value={formData.number} required onChange={handleChange}></input>
+                    </label>
+                </div>
+            </form>
+
+            <div className='passenger-footer'>
+                <button className='passenger-button' onClick={handleNext} disabled={!isFormValid}>Далее</button>
+            </div>
         </div>
     )
 }
